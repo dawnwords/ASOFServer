864f4363ce7ce1a107aa2730c4a8a3a26acb1ff3
Jingxiao Gu <gujingxiao@gmail.com>
2014-04-19 21:36:09 +0800

    + template class in the Response of TemplateServlet

--- src/edu/fudan/se/service/template/get/Response.java
+++ src/edu/fudan/se/service/template/get/Response.java
@@ -2,6 +2,15 @@ package edu.fudan.se.service.template.get;
 
 public class Response {
 	private byte[] file;
+	private String clazz;
+	
+	public String getClazz() {
+		return clazz;
+	}
+
+	public void setClazz(String clazz) {
+		this.clazz = clazz;
+	}
 
 	public byte[] getFile() {
 		return file;
--- src/edu/fudan/se/service/template/get/TemplateProvider.java
+++ src/edu/fudan/se/service/template/get/TemplateProvider.java
@@ -1,15 +1,38 @@
 package edu.fudan.se.service.template.get;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+import java.util.jar.Attributes.Name;
 
 import edu.fudan.se.service.servlet.util.IOUtil;
 import edu.fudan.se.service.servlet.util.Parameter;
 
 public class TemplateProvider {
+	private static final Name TEMPLATE_CLASS = new Name("Template-Class");
+
 	public static Response getTemplate(String name) {
 		Response response = new Response();
-		response.setFile(IOUtil.getBundleFile(Parameter.TEMPLATE_DIR
-				+ File.separator + name));
+		String path = Parameter.TEMPLATE_DIR + File.separator + name;
+		response.setFile(IOUtil.getBundleFile(path));
+		response.setClazz(getClassFromManifest(path));
 		return response;
+	}
+
+	private static String getClassFromManifest(String path) {
+		JarFile template = null;
+		try {
+			template = new JarFile(path);
+			Manifest manifest = template.getManifest();
+			Attributes attrs = (Attributes) manifest.getMainAttributes();
+			return attrs.getValue(TEMPLATE_CLASS);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			IOUtil.close(template);
+		}
+		return null;
 	}
 }
